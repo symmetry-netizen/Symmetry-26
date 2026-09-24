fd1e5119a6c370ab81a97e2eaca089386fa9401b
Symmetry-math <symmetry.presiunivmathematics@gmail.com>
2026-09-24 17:10:53 +0530

    Update form.js

--- web2site/js/form.js
+++ web2site/js/form.js
@@ -1,7 +1,7 @@
 // ==========================================
 // DEADLINE CONTROLS (KILL SWITCHES)
 // ==========================================
-const REGISTRATION_OPEN = false; 
+const REGISTRATION_OPEN = true; 
 const UPLOADS_OPEN = false;
 /* =========================================================
    SYMMETRY 2026
